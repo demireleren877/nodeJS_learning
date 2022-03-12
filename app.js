cdf91acbf338845fd2c8ac906856ff46569a4b29
eren demirel <l5819033@std.yildiz.edu.tr>
2022-03-13 00:29:50 +0300

    mongo learning started.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const { title } = require('process');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+
+const dbUrl = "mongodb+srv://de877:whilede877@cluster0.pv6in.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to database"))
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,6 +15,24 @@ app.set('view engine', 'ejs');
 
 app.listen(3000,"localhost",()=>{
     console.log("listening on port 3000");
+})
+
+app.get("/add-blog",(req,res)=>{
+    const blog = new Blog({
+        title: "My first blog",
+        snippet: "This is my first blog",
+        body: "This is my first blog"
+    })
+    blog.save().then((result)=>{
+        res.send(result);
+    })
+})
+
+
+app.get("/get-blogs",(req,res)=>{
+    Blog.find().then((result)=>{
+        res.send(result);
+    })
 })
 
 app.get("/",(req,res)=>{
